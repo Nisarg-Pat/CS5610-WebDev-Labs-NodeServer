15d6edb70ce7a15a13c1cc031f51d39b407368e2
Nisarg Patel <patel321nisarg@gmail.com>
2022-11-07 17:27:02 -0500

    Changes in mongoose connect

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/webdev');
+//mongoose.connect('mongodb://127.0.0.1:27017/webdev');
+mongoose.connect('mongodb+srv://CS5610-WebDev-Labs-DB:x0m78kw6uau57lqz@cluster0.hletlbg.mongodb.net/?retryWrites=true&w=majority');
 
 const express = require('express');
 const app = express();
